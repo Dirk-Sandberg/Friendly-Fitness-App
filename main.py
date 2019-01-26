1ffbe2a8c45c943d3d67189e76fa36b868a78476
ESandberg <37519914+dirk-sandberg@users.noreply.github.com>
2019-01-26 16:30:10 -0700

    After video series part 2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
 
 class HomeScreen(Screen):
     pass
+
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
 class SettingsScreen(Screen):
     pass
 
 
-GUI = Builder.load_file("main.kv")
+GUI = Builder.load_file("main.kv")  # Make sure this is after all class definitions!
 class MainApp(App):
     def build(self):
         return GUI
@@ -16,8 +23,6 @@ class MainApp(App):
     def change_screen(self, screen_name):
         # Get the screen manager from the kv file
         screen_manager = self.root.ids['screen_manager']
-        screen_manager.transition
         screen_manager.current = screen_name
-        #screen_manager = self.root.ids
 
 MainApp().run()
