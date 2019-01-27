914694594151075dedb6b8b7c32420793ab2741a
ESandberg <37519914+dirk-sandberg@users.noreply.github.com>
2019-01-26 20:26:47 -0700

    After video series part 4

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
+import requests
+import json
+
 
 class HomeScreen(Screen):
     pass
@@ -11,14 +14,30 @@ class HomeScreen(Screen):
 class ImageButton(ButtonBehavior, Image):
     pass
 
+
 class SettingsScreen(Screen):
     pass
 
 
 GUI = Builder.load_file("main.kv")  # Make sure this is after all class definitions!
 class MainApp(App):
+    my_friend_id = 1
     def build(self):
+
         return GUI
+
+    def on_start(self):
+        # Get database data
+        result = requests.get("https://friendly-fitness.firebaseio.com/" + str(self.my_friend_id) + ".json")
+        data = json.loads(result.content.decode())
+        workouts = data['workouts'][1:]
+        for workout in workouts:
+            print(workout['workout_image'])
+            print(workout['units'])
+
+        # Populate workout grid in home screen
+
+
 
     def change_screen(self, screen_name):
         # Get the screen manager from the kv file
